db47b30b82e85ae4a4aa8a1c48392f042d5f85fb
Iwan Timmer <irtimmer@gmail.com>
2017-05-06 22:14:19 +0200

    Add support for SHA256

--- tpm2-pk11/tpm.c
+++ tpm2-pk11/tpm.c
@@ -19,6 +19,9 @@
 
 #include "tpm.h"
 
+const unsigned char oid_sha1[] = {0x30, 0x21, 0x30, 0x09, 0x06, 0x05, 0x2B, 0x0E, 0x03, 0x02, 0x1A, 0x05, 0x00, 0x04, 0x14};
+const unsigned char oid_sha256[] = {0x30, 0x31, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01, 0x05, 0x00, 0x04, 0x20};
+
 TPM_RC tpm_sign(TSS2_SYS_CONTEXT *context, TPMI_DH_OBJECT handle, unsigned char *hash, unsigned long hashLength, TPMT_SIGNATURE *signature) {
   TPMS_AUTH_COMMAND sessionData;
   sessionData.hmac.t.size = 0;
@@ -47,12 +50,21 @@ TPM_RC tpm_sign(TSS2_SYS_CONTEXT *context, TPMI_DH_OBJECT handle, unsigned char 
 
   TPMT_SIG_SCHEME scheme;
   scheme.scheme = TPM_ALG_RSASSA;
-  scheme.details.rsassa.hashAlg = TPM_ALG_SHA1;
+
+  int digestSize;
+  if (memcmp(hash, oid_sha1, sizeof(oid_sha1)) == 0) {
+    scheme.details.rsassa.hashAlg = TPM_ALG_SHA1;
+    digestSize = SHA1_DIGEST_SIZE;
+  } else if (memcmp(hash, oid_sha256, sizeof(oid_sha256)) == 0) {
+    scheme.details.rsassa.hashAlg = TPM_ALG_SHA256;
+    digestSize = SHA256_DIGEST_SIZE;
+  } else
+    return TPM_RC_FAILURE;
 
   TPM2B_DIGEST digest = {0};
-  digest.t.size = digest.b.size = SHA1_DIGEST_SIZE;
+  digest.t.size = digest.b.size = digestSize;
   // Remove OID from hash if provided
-  memcpy(digest.t.buffer, hash - SHA1_DIGEST_SIZE + hashLength, hashLength);
+  memcpy(digest.t.buffer, hash - digestSize + hashLength, hashLength);
 
   return Tss2_Sys_Sign(context, handle, &sessionsData, &digest, &scheme, &validation, signature, &sessionsDataOut);
 }
