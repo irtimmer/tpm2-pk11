2d3d1931eb991f5536630c0709915ab4e16ab4b0
Iwan Timmer <irtimmer@gmail.com>
2018-01-23 18:33:41 +0100

    Add date and time to logs

--- src/log.c
+++ src/log.c
@@ -23,6 +23,7 @@
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 
 static int log_level = NONE;
 static FILE* log_stream = NULL;
@@ -41,9 +42,14 @@ void log_init(char* filename, int level) {
 
 void print_log(int level, const char* format, ...) {
   if (log_stream != NULL && level <= log_level) {
+    char buffer[20];
+    time_t now = time(0);
+    struct tm* time_now = localtime(&now);
+
     va_list args;
     va_start(args, format);
-    fprintf(log_stream, "[tpm-pk11] ");
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", time_now);
+    fprintf(log_stream, "%s [tpm-pk11] ", buffer);
     vfprintf(log_stream, format, args);
     fprintf(log_stream, "\n");
     va_end(args);
